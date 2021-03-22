8bc960486d3c4ab5af76bd49c1976d98d3e22e60
Gabriel Visconti <gabrielvisconti@icloud.com>
2021-03-22 10:14:58 -0300

    details

--- src/app/dialogs/add-comic/add-comic.component.ts
+++ src/app/dialogs/add-comic/add-comic.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
+import { AuthService } from '../../shared/services/auth/auth.service';
+import { MarvelService } from '../../shared/services/marvel/marvel.service';
+
 @Component({
   selector: 'app-add-comic',
   templateUrl: './add-comic.component.html',
@@ -8,18 +11,47 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class AddComicComponent implements OnInit {
 
+    loading = false;
+	response: any = {
+		offset: 0,
+		limit: 20,
+		count: 20,
+		total: 0
+	};
+	comics: Array<any> = [];
+
 	selectedComic: object = null;
 	condition: string = null;
 	price: string = null;
 	description: string = null;
 
-    constructor(public bsModalRef: BsModalRef) {}
+    constructor(
+		public bsModalRef: BsModalRef,
+		public authService: AuthService,
+		public marvelService: MarvelService,
+	) {
+		this.getComics();
+	}
 
     ngOnInit(): void {
     }
 
 	cleanSelection() {
 		this.selectedComic = null;
+	}
+
+	getComics() {
+        this.loading = true;
+		this.marvelService.getComics(this.response.limit, this.response.offset + 20)
+	  	.toPromise()
+			.then(data => {
+				this.loading = false;
+				this.response = data.data;
+				this.comics = this.comics.concat(data.data.results);
+			}, err => {
+				this.loading = false;
+				console.log(err);
+			});
 	}
 
 }
